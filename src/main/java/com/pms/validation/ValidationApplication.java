abb6076bfec4eb1e15dbc074e56f3beb0d006610
JAASWANTH07 <jaaswanth333@gmail.com>
2025-11-26 15:25:05 +0530

    first commit

---
+++ src/main/java/com/pms/validation/ValidationApplication.java
@@ -0,0 +1,16 @@
+package com.pms.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+})
+public class ValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationApplication.class, args);
+	}
+
+}
