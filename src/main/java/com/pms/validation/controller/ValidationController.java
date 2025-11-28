717971923dfd3232b0253c5d7186887a8529ee14
JAASWANTH07 <jaaswanth333@gmail.com>
2025-11-28 17:00:11 +0530

    merge fix

--- src/main/java/com/pms/validation/controller/ValidationController.java
+++ src/main/java/com/pms/validation/controller/ValidationController.java
@@ -1,10 +1,33 @@
 package com.pms.validation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pms.validation.entity.ValidationOutbox;
+import com.pms.validation.service.ValidationOutboxService;
+
+
 
 @RestController
 @RequestMapping("/validation")
 public class ValidationController {
+
+    @Autowired
+    private ValidationOutboxService validationOutboxService;
+
+    @Autowired
+    private KafkaTemplate<String, ValidationOutbox> kafkaTemplate;
     
+    @PostMapping("/validate/trade")
+    public void validateTrade(@RequestBody ValidationOutbox validationOutbox) {
+        // ValidationOutbox validatedTrade = validationOutboxService.validateTrade(validationOutbox);
+        // return ResponseEntity.ok(validatedTrade);
+
+        kafkaTemplate.send("ingestion-topic", validationOutbox.getPortfolioId().toString(), validationOutbox);
+    }
+
 }
--- src/main/java/com/pms/validation/service/ValidationOutboxService.java
+++ src/main/java/com/pms/validation/service/ValidationOutboxService.java
@@ -1,8 +1,63 @@
 package com.pms.validation.service;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import com.pms.validation.dao.ValidationOutboxDao;
+import com.pms.validation.entity.ValidationOutbox;
 
 @Service
 public class ValidationOutboxService {
+
+    @Autowired
+    private RedisTemplate<String, UUID> redisTemplate;
+
+    @Autowired
+    private KafkaTemplate<String, ValidationOutbox> kafkaTemplate;
+
+    @Autowired
+    private ValidationOutboxDao validationOutboxDao;
+
+    private static final String TRADE_SET = "ProcessedTrades";
     
+    public void validateTrade(ValidationOutbox validationOutbox) {
+
+        if(isDuplicateTrade(validationOutbox.getTradeId()))
+        {
+            System.out.println("Duplicate trade detected: " + validationOutbox.getTradeId());
+            return;
+        }
+
+        validationOutbox.setStatus("PENDING");
+
+        ValidationOutbox savedOutbox = validationOutboxDao.save(validationOutbox);
+
+        System.out.println("Saved to DB: " + savedOutbox);
+
+        kafkaTemplate.send("validation-topic",savedOutbox.getPortfolioId().toString(), savedOutbox);
+            // .addCallback(success -> {
+            //     System.out.println("Published to Kafka: " + savedOutbox);
+                
+            // },
+            // failure -> {
+            
+            // });
+
+    }
+
+    public boolean isDuplicateTrade(UUID tradeId) {
+
+        boolean exists = redisTemplate.opsForSet().isMember(TRADE_SET, tradeId);
+
+        if (exists) {
+            return true;
+        }
+
+        redisTemplate.opsForSet().add(TRADE_SET, tradeId);
+        return false;
+    }
 }
