464afffa85a70d91dd64ee3bc05654ee7fb641d9
sureshvasantha <sureshrbe22@gmail.com>
2025-12-29 13:26:11 +0530

    make use of pms_stocks instead of portfolio_symbol_details

---
+++ src/main/java/com/pms/validation/entity/StockEntity.java
@@ -0,0 +1,51 @@
+package com.pms.validation.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "pms_stocks")
+public class StockEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stock_id", nullable = false)
+    private Long stockId;
+
+    @Column(name = "symbol", nullable = false, unique = true)
+    private String symbol;
+
+    @Column(name = "sector_name", nullable = false)
+    private String sectorName;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
--- src/main/java/com/pms/validation/repository/StockRepository.java
+++ src/main/java/com/pms/validation/repository/StockRepository.java
@@ -3,9 +3,9 @@ package com.pms.validation.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.pms.validation.entity.SymbolEntity;
+import com.pms.validation.entity.StockEntity;
 
 @Repository
-public interface SymbolRepository extends JpaRepository<SymbolEntity, Long> {
+public interface StockRepository extends JpaRepository<StockEntity, Long> {
 
 }
